9a3818cc99a9bae9875bd50125231c627d56c751
Kate Hayes <katherine.hayes@ucdenver.edu>
2024-10-03 18:21:56 -0500

    finally got new output in - looks different with filter

--- testing diff.R
+++ testing diff.R
@@ -1,20 +1,19 @@
-model <- read.csv("/Users/katherinehayes/Google Drive/Projects/NSF Postdoc/Manuscripts/ConwayAgeGrowth/data/model_data.csv")
+model <- read.csv("/Users/katherinehayes/Google Drive/Projects/NSF Postdoc/Manuscripts/ConwayAgeGrowth/data/model output/model_data.csv")
 
-prefire <- read.csv("/Users/katherinehayes/Google Drive/Projects/NSF Postdoc/Manuscripts/ConwayAgeGrowth/data/prefire.csv")
-
+model_old <- read.csv("/Users/katherinehayes/Google Drive/Projects/NSF Postdoc/Manuscripts/ConwayAgeGrowth/data/model output/model_data_old.csv")
 
 
 test_model = model %>%
   filter(Scenario == "No Browsing") %>%
-  mutate(treat = "No filter")
-
-test_pre = prefire %>%
-  filter(Scenario == "No Browsing") %>%
   mutate(treat = "filter")
 
-test <- full_join(test_model, test_pre )
+test_old = model_old  %>%
+  filter(Scenario == "No Browsing") %>%
+  mutate(treat = "no filter")
+
+test <- full_join(test_model, test_old)
 
 test %>%
   filter(variable == "height") %>%
-  filter(species == "Bene") %>%
+  filter(species == "Pima") %>%
   ggplot(aes(x = TSF, y = mean, col = treat)) + geom_point()
